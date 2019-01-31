b8a592dd2e36e29781aca992eb3feac3234f3c1f
Preston Ryan <20.preston.ryan@ksd.org>
2019-01-31 13:01:34 -0800

    added player move script and playerHP

---
+++ Unity Mobile/Mobile Project/Assets/Scripts/PlayerHP.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+    public int health = 1;
+    public Text HealthText;
+    public Slider healthBar;
+    private void Start()
+    {
+        HealthText.GetComponent<Text>().text =
+            "Health: " + health;
+        healthBar.GetComponent<Slider>().value = health;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            health--;
+            HealthText.GetComponent<Text>().text =
+               "Health: " + health;
+            healthBar.GetComponent<Slider>().value = health;
+        }
+        if (health <= 0)
+        {
+            //reload the level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+
+}
---
+++ Unity Mobile/Mobile Project/Assets/Scripts/moving.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileMovement : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    public float jumpSpeed = 1.0f;
+    private bool grounded = false;
+    private float moveDir = 0;
+    // Use this for initialization
+    void Start() { }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        Vector3 velocity = GetComponent<Rigidbody2D>().velocity;
+        velocity.x = moveSpeed * moveDir;
+        GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+    public void MoveRight()
+    {
+        moveDir = 1;
+    }
+    public void MoveLeft()
+    {
+        moveDir = -1;
+    }
+    public void Stop()
+    {
+        moveDir = 0;
+    }
+    public void Jump()
+    {
+        if (grounded)
+        {
+            GetComponent<Rigidbody2D>().AddForce(
+                new Vector2(0, 100 * jumpSpeed));
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            grounded = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            grounded = false;
+        }
+    }
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            grounded = true;
+        }
+    }
+}

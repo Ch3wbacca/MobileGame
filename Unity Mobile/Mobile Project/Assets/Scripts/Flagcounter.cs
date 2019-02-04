6be923f35867d1403cca96105519b721f41ab176
Preston Ryan <20.preston.ryan@ksd.org>
2019-02-04 13:56:06 -0800

    worked in easy level

---
+++ Unity Mobile/Mobile Project/Assets/Scripts/Flagcounter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flagcounter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text Flags;
+    public Text FlagCounter;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Flag")
+        {
+            Flags.GetComponent<Text>().text =
+            "Flag: " + Flags;
+        }
+    }
+    
+
+}
+
+
+
+
+

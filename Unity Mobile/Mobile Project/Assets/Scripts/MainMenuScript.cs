14c9977ed0df40b1f107db248ca5381754e9470a
altiris altiris <altiris@ksd.edu>
2019-02-04 13:33:08 -0800

    Main Menu Script Added

---
+++ Unity Mobile/Mobile Project/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void Easy()
+    {
+        SceneManager.LoadScene("Easy");
+    }
+
+    public void Normal()
+    {
+        SceneManager.LoadScene("Medium");
+    }
+
+    public void Hard()
+    {
+        SceneManager.LoadScene("Hard");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}

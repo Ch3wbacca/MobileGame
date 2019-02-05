a85d5d298318922f8c7b4263397666a949fe5d78
Preston Ryan <20.preston.ryan@ksd.org>
2019-02-05 12:07:32 -0800

    Messing with flagcounter script

---
+++ Unity Mobile/Mobile Project/Assets/Scripts/Delete.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delete : MonoBehaviour
+{
+    private void OnCollisionExit(Collision collision)
+    {
+        Destroy(gameObject.tag "useless");
+    }
+
+}

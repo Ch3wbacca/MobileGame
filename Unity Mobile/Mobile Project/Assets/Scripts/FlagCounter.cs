12984693f33f28c7294f42bbaec0132b702b82fb
Preston Ryan <20.preston.ryan@ksd.org>
2019-02-07 11:28:56 -0800

    made prefabs for ground and player, working on flag and respawning and despawning objects

--- Unity Mobile/Mobile Project/Assets/Scripts/FlagCounter.cs
+++ Unity Mobile/Mobile Project/Assets/Scripts/FlagCounter.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class FlagCounter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int Flag = 0;
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "Flag")
+        {
+            Flag++;
+            Destroy(collision.gameObject);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }

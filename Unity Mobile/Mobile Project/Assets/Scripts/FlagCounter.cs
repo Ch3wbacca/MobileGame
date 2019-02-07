1bf806dcd5cef6cdebb780562c8f91b395361954
Preston Ryan <20.preston.ryan@ksd.org>
2019-02-07 13:58:03 -0800

    worked in level easy and started making presets for platforms

--- Unity Mobile/Mobile Project/Assets/Scripts/FlagCounter.cs
+++ Unity Mobile/Mobile Project/Assets/Scripts/FlagCounter.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 public class FlagCounter : MonoBehaviour
 {
-    public int Flag = 0;
-
+    public Text Flag;
+    public int Flagcounter = 0;
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Flag")
         {
-            Flag++;
+            Flagcounter += 1;
+            Flag.GetComponent<Text>().text =
+                "Flag " + Flagcounter;
             Destroy(collision.gameObject);
         }
 

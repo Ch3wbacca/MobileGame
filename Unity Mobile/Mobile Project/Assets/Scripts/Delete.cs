9e4e6cacfcb7ec6e80de2de7bc9262d4e40efce1
Preston Ryan <20.preston.ryan@ksd.org>
2019-02-19 13:51:58 -0800

    made level 1 Easy Prefabs and working on RandomPlatformer Script

--- Unity Mobile/Mobile Project/Assets/Scripts/Delete.cs
+++ Unity Mobile/Mobile Project/Assets/Scripts/Delete.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Delete : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(transform.parent.gameObject);
     }
 }
---
+++ Unity Mobile/Mobile Project/Assets/Scripts/RandomPlatform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPlatform : MonoBehaviour
+{
+    public GameObject prefab1, prefab2, prefab3, prefab4;
+    int WhatToSPawn;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        WhatToSPawn = Random.Range(1, 5);
+        switch (WhatToSPawn)
+        {
+            case 1:
+                Instantiate(prefab1, transform.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(prefab2, transform.position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(prefab3, transform.position, Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(prefab4, transform.position, Quaternion.identity);
+                break;
+        }
+    }
+}

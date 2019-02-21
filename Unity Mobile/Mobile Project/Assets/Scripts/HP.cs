7d68d801151ecd8619af4f5bc0356b2171114a37
William Relaford <20.william.relaford@ksd.org>
2019-02-21 12:44:25 -0800

    Player Hud and death update

--- Unity Mobile/Mobile Project/Assets/Scripts/HP.cs
+++ Unity Mobile/Mobile Project/Assets/Scripts/HP.cs
@@ -11,7 +11,7 @@ public class HP : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Player")
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }

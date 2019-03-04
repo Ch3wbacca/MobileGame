440d4f72e3302271c0308b9fd53db4fa46a6da35
Preston Ryan <20.preston.ryan@ksd.org>
2019-03-04 13:00:36 -0800

    added pause to medium and easy, put art on main menu

---
+++ Unity Mobile/Mobile Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+
+                //make the menu/buttons appear 
+                GetComponent<Canvas>().enabled = true;
+                //pause the game
+                Time.timeScale = 0;
+            }
+            else if (Time.timeScale == 0)
+
+            {
+                Resume();
+
+            }
+
+
+        }
+    }
+    public void Resume()
+    {
+        GetComponent<Canvas>().enabled = false;
+        Time.timeScale = 1;
+    }
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Application.Quit");
+    }
+
+
+}
